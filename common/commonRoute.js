7087d21b7e80b2118e836bf9b40d36c71468a01f
rrshub <rani8014@gmail.com>
2023-04-21 11:23:55 +0530

    getGradeCategories api name changed

--- common/commonRoute.js
+++ common/commonRoute.js
@@ -4,7 +4,7 @@ const commonRoute = express.Router();
 commonRoute.use( '/getSyllabuses',require('../authentication/validateToken'),require('../syllabus/getSyllabus'))
 commonRoute.use( '/deleteSyllabus',require('../authentication/validateToken'),require('../syllabus/deleteSyllabus'))
 commonRoute.use( '/createSyllabus',require('../authentication/validateToken'),require('../syllabus/insertSyllabus'))
-commonRoute.use( '/gradeCategories',require('../authentication/validateToken'),require('../gradeCategory/getGradeCategory'))
+commonRoute.use( '/getGradeCategories',require('../authentication/validateToken'),require('../gradeCategory/getGradeCategory'))
 commonRoute.use( '/getRoles',require('../authentication/validateToken'),require('../role/getRoles'))
 commonRoute.use( '/getUserTypes',require('../authentication/validateToken'),require('../userType/getUserTypes'))
 
