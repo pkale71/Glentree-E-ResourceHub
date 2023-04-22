7ca5ba8ec2a23c3e52445fb0d133389c37002cf5
rrshub <rani8014@gmail.com>
2023-04-22 18:36:37 +0530

    get schools

---
+++ school/getSchool.js
@@ -0,0 +1,61 @@
+let db = require('./databaseQuerySchool')
+let schoolObj = require('../models/school')
+let errorCode = require('../common/errorCode')
+let getCode = new errorCode()
+let schools = new schoolObj()
+let school;
+let schoolList = [];
+let schoolUUID;
+
+module.exports = require('express').Router().get('/:schoolUUID',async(req,res) =>  {
+    try
+    {
+        schoolUUID = req.params.schoolUUID
+        console.log(schoolUUID)
+        school = await db.getSchool(schoolUUID)
+         await Object.keys(school[0]).forEach(function(key) {
+            school = JSON.parse(school[0][key]);
+            // ...
+        })
+        return res.json({
+            "status_code"   :   404,
+            "data"          :   {'school' : school },
+            "message"       :   'success',
+            "status_name"   :   getCode.getStatus(404),
+        })  
+        schoolList = [];
+        if(school.length == 0){
+            res.status(200)
+            return res.json({
+                "status_code"   :   404,
+                "data"          :   {'school' : []},
+                "message"       :   'success',
+                "status_name"   :   getCode.getStatus(404),
+            })   
+        }
+        await Array.from(school).forEach(ele  =>  {
+            schools.setDataAll(ele)
+            schoolList.push(schools.getDataAll())
+        })
+
+        if(school.length == schoolList.length){
+            res.status(200)
+            return res.json({
+                "status_code" : 200,
+                "data"        : {'school' : schoolList},
+                "message"     : 'success',
+                status_name   : getCode.getStatus(200)
+            })
+        } 
+    } 
+    catch(e)
+    {
+        res.status(500)
+        return res.json({
+            "status_code"   :   500,
+            "message"       :   "School not found",
+            "status_name"   :   getCode.getStatus(500),
+            "error"         :   e.sqlMessage
+        })     
+    }
+})
