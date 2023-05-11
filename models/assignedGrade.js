4c90454d6441a1dcd25e19d7b7d01e229280a472
rrshub <rani8014@gmial.com>
2023-05-11 08:11:10 +0530

    getAssignedGrade.js done

---
+++ models/assignedGrade.js
@@ -0,0 +1,65 @@
+class unassignedGrade {
+    id
+    name
+    userSuperviseGrades
+
+
+    constructor(){}
+
+    setSuperviseGrade(data){
+        this.userSuperviseGrades = {
+            "uuid" : data.uuid,
+            "user" : {
+                "uuid" : data.userUuid,
+                "name" : data.userName.trim()
+            },
+            "assignedGrade" : {
+                "id" : data.gradeId,
+                "name" : data.gradeName
+            },
+            "school" : {
+                "uuid" : data.schoolUuid,
+                "name" : data.schoolName
+            },
+            "academicYear" : {
+                "uuid" : data.acaUuid,
+                "year" : data.year
+            }
+        }
+    }
+
+    getSuperviseGrade(){
+        return this.userSuperviseGrades
+    }
+    
+    setDataAll(data)
+    {
+        this.id     =   data.id
+        this.name   =   data.name?.trim()
+    }
+
+    getDataAll()
+    {
+        return {
+            id : this.id,
+            name : this.name
+        }
+    }
+
+    setData(data)
+    {
+        this.id     =   data.gradeCategoryId
+        this.name   =   data.gradeCategoryName?.trim()
+        this.userSuperviseGrades = data.userSuperviseGrades
+    }
+
+    getData()
+    {
+        return {
+            id : this.id,
+            name : this.name,
+            userSuperviseGrades : this.userSuperviseGrades
+        }
+    }
+}
+module.exports = unassignedGrade
--- userRoute/userSuperviseGrade/getAssignedGrade.js
+++ userRoute/userSuperviseGrade/getAssignedGrade.js
@@ -1,83 +1,50 @@
 let db = require('./databaseQuerySuperviseGrade')
-let unassignedGrade = require('../../models/unassignedGrade')
+let assignedGrade = require('../../models/assignedGrade')
 let errorCode = require('../../common/errorCode')
 const school = require('../../models/school')
 let getCode = new errorCode()
-let grade = new unassignedGrade()
-let topUuid;
-let chapter
-let grades;
-let schoolUuid;
+let assignGrade = new assignedGrade()
 let acaUuid;
-let gradeCategoryId
-let ids
-let gradeList = []
+let userUuid
+let AssignGrade
+let assignGradeList = []
+let assignList = []
 
 module.exports = require('express').Router().get('/:userUuid/:acaUuid',async(req,res)=>{
     try{
         acaUuid =  req.params.acaUuid
         userUuid = req.params.userUuid
-        
-       // ids = await db.findSchoolAndAcaId(acaUuid,0,userUuid)
-       
-       console.log(acaUuid,userUuid)
-        
-            // acaId = ids[0]['acaId']
-            // schoolId = ids[0]['schoolId']
-            // userId = ids[0]['userId']
-            // userName = ids[0]['userName']
-            AssignGrade = await db.findSchoolGradeCategory(userUuid,acaUuid)
-            console.log(AssignGrade)
-            return
-            if(grades.length == 0){
-                res.status(400)
-                return res.json({
-                    "status_code" : 400,
-                    "message"     : 'No unassigned grade found',
-                    "status_name"   : getCode.getStatus(400)
-                })
+        AssignGrade = await db.findSchoolGradeCategory(userUuid,acaUuid)
+        if(AssignGrade.length == 0)
+        {
+            res.status(400)
+            return res.json({
+                "status_code" : 400,
+                "message"     : 'No assigned grade found',
+                "status_name"   : getCode.getStatus(400)
+            })
+        }
+        else
+        {
+            assignGradeList = []
+            assignList = []
+            Array.from(AssignGrade).forEach(( ele ) =>  
+            {
+                assignGrade.setSuperviseGrade(ele)
+                assignGradeList.push(assignGrade.getSuperviseGrade())   
+            })
+            
+            AssignGrade[0]['userSuperviseGrades'] = assignGradeList
+            assignGrade.setData(AssignGrade[0])
+            assignList.push(assignGrade.getData())
+            res.status(200)
+            return res.json({
+                "status_code" : 200,
+                "data"        : {'assignedGrades' : assignList},
+                "message"     : 'success',
+                "status_name"   : getCode.getStatus(200)
+            })
             }
-            else{
-                gradeList = []
-                Array.from(grades).forEach(async( ele ) =>  {
-                    grade.setDataAll(ele)
-                    gradeList.push(grade.getDataAll()) 
-                      if(gradeList.length == grades.length){
-                          res.status(200)
-                          return res.json({
-                              "status_code" : 200,
-                              "data"        : {'unassignedGrades' : gradeList},
-                              "message"     : 'success',
-                              "status_name"   : getCode.getStatus(200)
-                          })
-                      } 
-                  })
-            }
-        
-       
-       
-        
-        // topicList = [];
-        // topList = []
-        // if(chapter.length == 0){
-        //     res.status(404)
-        //     return res.json({
-        //         "status_code"   :   404,
-        //         "message"       :   'Chapter not found',
-        //         "status_name"   :   getCode.getStatus(404),
-        //     })   
-        // }
-        // topics = await db.getChapterTopics(chapter[0].id,0)
-        // if(topics.length == 0){
-        //     res.status(200)
-        //     return res.json({
-        //         "status_code" : 200,
-        //         "data"        : {'chapterTopics' : []},
-        //         "message"     : 'success',
-        //         "status_name"   : getCode.getStatus(200)
-        //     })
-        // }
-          
         } 
         catch(e)
         {
@@ -85,7 +52,7 @@ module.exports = require('express').Router().get('/:userUuid/:acaUuid',async(req
             res.status(500)
                 return res.json({
                     "status_code" : 500,
-                    "message" : "Unassigned grade not found",
+                    "message" : "Assigned grade not found",
                     "status_name" : getCode.getStatus(500),
                     "error"     :      e.sqlMessage
                 }) 
