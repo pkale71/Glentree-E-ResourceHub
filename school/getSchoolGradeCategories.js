19c39df84ad479fb3694c7ee8fc2db92d18389a7
Prashant Kale <pkale71@gmail.com>
2023-05-23 17:46:56 +0530

    Update getSchoolGradeCategories.js

--- school/getSchoolGradeCategories.js
+++ school/getSchoolGradeCategories.js
@@ -39,7 +39,6 @@ module.exports = require('express').Router().get('/:uuid',async(req,res) =>  {
     } 
     catch(e)
     {
-        console.log(e);
         res.status(500)
         return res.json({
             "status_code"   :   500,
